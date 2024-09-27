24e0cc560b96feba64d6dc4957a6dc8192007ade
Stephen <stephennguyen24@gmail.com>
2024-09-28 05:37:14 +0700

    chore: add Promise interfance

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -1,1 +1,31 @@
+import { Pizza, PizzaProp } from "./models/Pizza";
 
+const titleInput = document.querySelector(
+  'input[name="title"]'
+) as HTMLInputElement;
+const descriptionInput = document.querySelector(
+  "textarea"
+) as HTMLTextAreaElement;
+
+const form = document.querySelector(".create") as HTMLFormElement;
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  const newPizza: PizzaProp = {
+    title: data.get("title") as string,
+    description: data.get("description") as string,
+    toppings: data.getAll("toppings") as string[],
+    price: parseInt(data.get("price") as string) || 10,
+  };
+
+  const res = await Pizza.save(newPizza);
+
+  if (res.ok) {
+    window.location.href = "/";
+  } else {
+    console.log("Fail to create new pizza");
+  }
+});
